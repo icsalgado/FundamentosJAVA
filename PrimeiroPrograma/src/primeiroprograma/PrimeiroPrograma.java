a6a2913afd02264f52774bb2f2cd51242796ca61
icsalgado <icsalgado@icloud.com>
2021-08-09 23:04:01 -0300

    Primeiro olá mundo em java

---
+++ PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package primeiroprograma;
+/*@author ICSalgado*/
+public class PrimeiroPrograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { //psvn
+        // TODO code application logic here
+        
+        System.out.print("Ola mundo");//sout
+        
+    }
+    
+}
